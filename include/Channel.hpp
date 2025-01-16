ebf8c8d305d2fcca9680635c4f04b79e8ba9e55f
mowhry <jskiba@student.42.fr>
2025-01-16 15:13:25 +0100

    add a basic implementation for channel

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -3,10 +3,23 @@
 #include "./Server.hpp"
 
 class Channel{
+private:
+	std::string			_name;
+	std::vector<Client *>	_users;
 
-	std::string name;
-	std::vector<Client> users;
+public:
+	Channel();
+	Channel(const Channel &copy);
+	Channel(const std::string &name);
+	~Channel();
 
+	Channel& operator=(const Channel &os);
 
+	std::string	getName() const;
+	void		setName(const std::string &name);
+
+	void		addUser(Client *user);
+	void		removeUser(Client *user);
+	const std::vector<Client *>& getUser() const;
 
 };
---
+++ src/Channel.cpp
@@ -0,0 +1,69 @@
+#include "../include/Channel.hpp"
+#include "algorithm"
+
+Channel::Channel() : _name(""), _users()
+{
+}
+
+Channel::Channel(const std::string &name) : _name(name), _users()
+{
+}
+
+Channel::Channel(const Channel &copy)
+{
+	this->_name = copy._name;
+	this->_users = copy._users;
+}
+
+Channel::~Channel()
+{
+}
+
+Channel& Channel::operator=(const Channel &other)
+{
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->_users = other._users; // copie les pointeurs (shallow copy)
+	}
+	return *this;
+}
+
+std::string Channel::getName() const
+{
+	return this->_name;
+}
+
+void Channel::setName(const std::string &name)
+{
+	this->_name = name;
+}
+
+
+void Channel::addUser(Client *user)
+{
+	for (size_t i = 0; i < _users.size(); i++)
+	{
+		if (_users[i] == user)
+			return;
+	}
+	_users.push_back(user);
+}
+
+void Channel::removeUser(Client *user)
+{
+	for (size_t i = 0; i < _users.size(); i++)
+	{
+		if (_users[i] == user)
+		{
+			_users.erase(_users.begin() + i);
+			break;
+		}
+	}
+}
+
+const std::vector<Client *>& Channel::getUser() const
+{
+	return this->_users;
+}
+
